b69f4362ad6a201ddf0eb0b670ccf658c51b881b
basschaser <59636191+basschaser@users.noreply.github.com>
2021-07-25 13:33:03 -0700

    Clean up networking configuration, add polkit rule for network group

--- airootfs/usr/local/lib/arch-grit/modules/chroot/20-setupNetwork.sh
+++ airootfs/usr/local/lib/arch-grit/modules/chroot/20-setupNetwork.sh
@@ -9,8 +9,27 @@ echo "127.0.0.1	localhost" >> /etc/hosts || exit 1
 echo "::1		localhost" >> /etc/hosts || exit 1
 echo "127.0.1.1	${HNAME:?}.localdomain	${HNAME:?}" >> /etc/hosts || exit 1
 
-# Enable networking services
+# Mask udev rule to get simple network interface names (eth0, wlan0)
+ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules || exit 1
+
+# Configure base networking services
+systemctl enable firewalld.service || exit 1
 systemctl enable NetworkManager.service || exit 1
+
+# Enable network management for 'network' group
+cat << EOF > /etc/polkit-1/rules.d/50-org.freedesktop.NetworkManager.rules || exit 1
+polkit.addRule(function(action, subject) {
+  if (action.id.indexOf("org.freedesktop.NetworkManager.") == 0 && subject.isInGroup("network")) {
+    return polkit.Result.YES;
+  }
+});
+EOF
+
+# Enable printing services
+systemctl enable  avahi-daemon.service || exit 1
+systemctl enable  cups.socket || exit 1
+systemctl disable systemd-resolved.service || exit 1
+sed -i "s/resolve/mdns_minimal [NOTFOUND=return] resolve/" /etc/nsswitch.conf
 
 
 exit 0
--- airootfs/usr/local/lib/arch-grit/modules/chroot/60-systemConfig.sh
+++ airootfs/usr/local/lib/arch-grit/modules/chroot/60-systemConfig.sh
@@ -2,24 +2,14 @@
 
 trap "exit 1" 1 2 3 SIGTRAP 6 14 15
 
-# Mask udev rule to get simple network interface names (eth0, wlan0)
-ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules || exit 1
-
 # Enable system services
 systemctl enable cpupower.service || exit 1
-systemctl enable firewalld.service || exit 1
 if [[ $SSD = 1 && $TRIM = 1 ]] ; then
 	systemctl enable fstrim.timer || exit 1
 fi
 systemctl enable lightdm-plymouth.service || exit 1
 systemctl enable paccache.timer || exit 1
 systemctl enable smartd.service || exit 1
-
-# Enable printing services
-systemctl enable  avahi-daemon.service || exit 1
-systemctl enable  cups.socket || exit 1
-systemctl disable systemd-resolved.service || exit 1
-sed -i "s/resolve/mdns_minimal [NOTFOUND=return] resolve/" /etc/nsswitch.conf
 
 # Sysctl - Tweaks/optimizations
 echo "kernel.sysrq = 1" > /etc/sysctl.d/80-sysrq.conf || exit 1
