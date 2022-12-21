b90bb35d498b8f887e0ea511dc48aa0f7a7f888d
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-12-20 16:08:24 -0800

    Fix: Update unmount module for ZFS

--- src/airootfs/usr/local/lib/arch-grit/modules/99-unmountFilesystems.sh
+++ src/airootfs/usr/local/lib/arch-grit/modules/99-unmountFilesystems.sh
@@ -5,11 +5,9 @@ unmountFilesystems() {
     # Remove installation files from new system
     rm -rf "/mnt/${libPath:?}" || return 1
 
-    # Unmount all filesystems to complete the installation
-    timeout=0
-    until umount -R /mnt ; do
-        sleep 5
-        timeout=$((timeout+5))
-        [[ $timeout = 60 ]] && exit 1
-    done
+    # Sync and unmount all filesystems to complete installation
+    sync || return 1
+    umount /mnt/boot || return 1
+    zfs umount -a || return 1
+    zpool export zroot || return 1
 }
