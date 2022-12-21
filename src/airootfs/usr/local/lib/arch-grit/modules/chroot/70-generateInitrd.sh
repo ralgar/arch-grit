f6c2621b7807f89662ebfaef19880952dbc346b3
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-12-20 23:38:01 -0800

    Generate hostid for initramfs

--- src/airootfs/usr/local/lib/arch-grit/modules/chroot/70-generateInitrd.sh
+++ src/airootfs/usr/local/lib/arch-grit/modules/chroot/70-generateInitrd.sh
@@ -5,6 +5,7 @@ trap "exit 1" 1 2 3 SIGTRAP 6 14 15
 
 # Configure and generate the initramfs
 sed -i "s/^HOOKS=.*/HOOKS=(base udev autodetect modconf block keyboard zfs filesystems)/" /etc/mkinitcpio.conf || exit 1
-mkinitcpio -P
+zgenhostid "$(hostid)" || exit 1
+mkinitcpio -P || exit 1
 
 exit 0
