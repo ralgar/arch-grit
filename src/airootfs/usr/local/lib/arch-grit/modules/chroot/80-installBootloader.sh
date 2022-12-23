3edf82ad87002d04c0aec2ab47a651149ae2fc4a
Ryan Algar <59636191+ralgar@users.noreply.github.com>
2022-12-22 21:46:52 -0800

    Fix: Plymouth not displaying on boot
    Forgot to set kernel cmdline `splash` argument

--- src/airootfs/usr/local/lib/arch-grit/modules/chroot/80-installBootloader.sh
+++ src/airootfs/usr/local/lib/arch-grit/modules/chroot/80-installBootloader.sh
@@ -4,8 +4,11 @@ trap "exit 1" 1 2 3 SIGTRAP 6 14 15
 
 uefiBootConfig() {
 	pacman -S --noconfirm grub efibootmgr "${cpuVendor:?}-ucode" || return 1
-    grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
+    grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB || return 1
 
+    cmdline_key="GRUB_CMDLINE_LINUX_DEFAULT" || return 1
+    cmdline_val="quiet splash loglevel=3" || return 1
+    sed -i "s/^$cmdline_key.*$/$cmdline_key=$cmdline_val/" /etc/default/grub || return 1
     grub-mkconfig -o /boot/grub/grub.cfg || return 1
 }
 
@@ -13,6 +16,9 @@ biosBootConfig() {
     pacman -S --noconfirm grub "${cpuVendor:?}-ucode" || return 1
     grub-install --target=i386-pc "${DRIVE:?}" || return 1
 
+    cmdline_key="GRUB_CMDLINE_LINUX_DEFAULT" || return 1
+    cmdline_val="quiet splash loglevel=3" || return 1
+    sed -i "s/^$cmdline_key.*$/$cmdline_key=$cmdline_val/" /etc/default/grub || return 1
     grub-mkconfig -o /boot/grub/grub.cfg || return 1
 }
 
