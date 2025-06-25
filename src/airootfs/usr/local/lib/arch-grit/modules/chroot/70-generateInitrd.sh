e0fb29bfaa2b32efc77d52c8f140df257ae4f118
Ryan Algar <me@ralgar.dev>
2025-06-24 21:28:29 -0700

    chore(initrd): update mkinitcpio.conf modules (#6)

--- src/airootfs/usr/local/lib/arch-grit/modules/chroot/70-generateInitrd.sh
+++ src/airootfs/usr/local/lib/arch-grit/modules/chroot/70-generateInitrd.sh
@@ -4,7 +4,7 @@
 trap "exit 1" 1 2 3 SIGTRAP 6 14 15
 
 # Configure and generate the initramfs
-sed -i "s/^HOOKS=.*/HOOKS=(base udev plymouth autodetect modconf block keyboard plymouth-zfs filesystems)/" /etc/mkinitcpio.conf || exit 1
+sed -i "s/^HOOKS=.*/HOOKS=(base udev plymouth autodetect microcode modconf kms keyboard keymap consolefont block plymouth-zfs filesystems)/" /etc/mkinitcpio.conf || exit 1
 zgenhostid "$(hostid)" || exit 1
 plymouth-set-default-theme -R arch-logo-new || exit 1
 
